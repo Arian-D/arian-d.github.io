a9f5d2946fef2db26109accdcefffad1cf188d2a
Arian-D <ArianXDehghani@gmail.com>
2022-11-15 04:32:57 -0800

    Added basic nix files for running `npm run develop'

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "My personal website";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShells.default = import ./shell.nix { inherit pkgs; };
+        }
+      );
+}
