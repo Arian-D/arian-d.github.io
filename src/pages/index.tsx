1f4f9fef2d41d8fb28b590f0351be3cadb60f8ba
Arian-D <ArianXDehghani@gmail.com>
2023-04-24 00:59:54 -0700

    Add title and enlarge font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import Link from 'next/link'
+import Head from 'next/head'
+
+const title = <title>Arian's Website</title>
 
 function Profiles() {
   const profiles = [
@@ -10,7 +13,7 @@ function Profiles() {
 
   const listItems = profiles
     .map((account) => <Link href={account[1]} key={account[1]}>{account[0]}</Link>)
-    .map((atag) => <li key={atag.key}>{atag}</li>)
+    .map((atag) => <li className="text-lg" key={atag.key}>{atag}</li>)
 
   return <ul>{listItems}</ul>
 }
@@ -18,7 +21,8 @@ function Profiles() {
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="font-bold text-xl">
+      <Head>{title}</Head>
+      <h1 className="font-bold text-5xl">
         Arian Dehghani
       </h1>
       <Profiles />
