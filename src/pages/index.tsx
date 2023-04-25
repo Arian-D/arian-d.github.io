905afa257e10e419b6728c0cedc50e777b31ae04
Arian-D <ArianXDehghani@gmail.com>
2023-04-25 01:19:38 -0700

    Make buttons bigger with icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,48 @@
 import Link from 'next/link'
 import Head from 'next/head'
+import { FaGithub, FaLinkedin } from "react-icons/fa"
+import { MdEmail, MdForum } from "react-icons/md"
 
-const title = <title>Arian's Website</title>
+const myName = "Arian Dehghani"
+
+const title = <title>{myName}</title>
+
+type Profile = {
+  name: string;
+  url: string;
+  icon: JSX.Element;
+}
+
+const profiles: Profile[] = [
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/arian-dehghani",
+    icon: <FaLinkedin />
+  },
+  {
+    name: "GitHub",
+    url: "https://github.com/arian-d",
+    icon: <FaGithub />
+  },
+  {
+    name: "Email",
+    url: "mailto:arianxdehghani@gmail.com",
+    icon: <MdEmail />
+  },
+  {
+    name: "Blog",
+    url: "https://arian-d.github.io/blog",
+    icon: <MdForum />
+  }
+]
 
 function Profiles() {
-  const profiles = [
-    ["LinkedIn", "https://www.linkedin.com/in/arian-dehghani"],
-    ["Email", "mailto:arianxdehghani@gmail.com"],
-    ["GitHub", "https://github.com/arian-d"],
-    ["Blog", "https://arian-d.github.io/blog"],
-  ]
-
   const listItems = profiles
-    .map((account) => <Link href={account[1]} key={account[1]}>{account[0]}</Link>)
-    .map((atag) => <li className="text-lg" key={atag.key}>{atag}</li>)
+    .map((account) =>
+      <Link href={account.url} key={account.name}> {account.icon}{account.name}
+      </Link>)
+    .map((atag) =>
+      <li className="my-2 p-2 text-2xl hover:animate-pulse border rounded-md" key={atag.key}>{atag}</li>)
 
   return <ul>{listItems}</ul>
 }
@@ -23,7 +52,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Head>{title}</Head>
       <h1 className="font-bold text-5xl">
-        Arian Dehghani
+        {myName}
       </h1>
       <Profiles />
     </main>
