28ce01e1d0924507fc0137926abee99457c63b2b
Arian-D <Arian-D@users.noreply.github.com>
2023-04-25 08:48:39 +0000

    deploy: 905afa257e10e419b6728c0cedc50e777b31ae04

---
+++ _next/static/JX201InTgd3xu5wvPX-0a/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/252f366e-27696bc11684427e.js","static/chunks/1bfc9850-ea719a2832119eaf.js","static/chunks/pages/index-61b74955bd8af1d7.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
